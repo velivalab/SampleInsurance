7ae8694d0cb99d175511cf14cd38bd0846ea0223
bvelivala <bhavaniv448@gmail.com>
2017-03-14 11:00:41 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ echo "Version to be downloaded is : $VERSION_NO"
 cd /tmp
 
 echo "Downloading autoclaim war version $VERSION_NO"
-wget "http://ec2-35-167-7-136.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/com/example/autoclaim/$VERSION_NO/autoclaim-$VERSION_NO.war"
+wget "http://ec2-35-166-232-252.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/com/example/autoclaim/$VERSION_NO/autoclaim-$VERSION_NO.war"
 
 if [ -f autoclaim-$VERSION_NO.war ]; then
         echo "Downloaded autoclaim war version $VERSION_NO"
@@ -17,11 +17,16 @@ if [ -f autoclaim-$VERSION_NO.war ]; then
         cp autoclaim-$VERSION_NO.war autoclaim.war
         echo "Copied autoclaim-$VERSION_NO.war to autoclaim.war"
 
+        echo "Undeploy previous version of autoclaim on Tomcat Server $TOMCAT_SERVER_URL"
+        curl -v --basic --user admin:devops "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/undeploy?path=/autoclaim&update=true"
+        echo "Undeploy autoclaim on Tomcat Server $TOMCAT_SERVER_URL is successful"
+        
         echo "Deploying autoclaim.war to Tomcat Server $TOMCAT_SERVER_URL"
         curl -v --basic --user admin:devops --upload-file autoclaim.war "http://manager-script:changeit@$TOMCAT_SERVER_URL/manager/text/deploy?path=/autoclaim&update=true"
-        echo "Deployment Successful"
+        echo "Deployment of autoclaim app binaries version $VERSION_NO on server $TOMCAT_SERVER_URL is Successful"
+        
         rm -f autoclaim*.war
         echo "Downloaded binaries deleted from tmp folder"
 else
-        echo "Deployment Failed because autoclaim binaries of version $VERSION_NO could not be found"
+        echo "Deployment of autoclaim app binaries version $VERSION_NO on server $TOMCAT_SERVER_URL Failed"
 fi
